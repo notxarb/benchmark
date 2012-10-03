fa3e61b523cb6c95522ad5f1cf606adc3f2fb4e3
Braxton Huggins <braxton@delorum.com>
2012-10-03 16:34:17 -0600

    add redis2.php

---
+++ redis2.php
@@ -0,0 +1,29 @@
+<?php
+  public static function genstring($size)
+  {
+    $string = ''
+    for ($i = 0; $i < $size; $i++)
+    {
+      $string .= $i % 10
+    }
+    return $string
+  }
+  $redis = new Redis();
+  $redis->connect($_SERVER['CACHE2_HOST'], $_SERVER['CACHE2_PORT']);
+  for($i = 64; $i < 4096; $i *= 2)
+  {
+    $in = genstring($i)
+    $redis->set("test".$size, $in)
+    $out = $redis->get("test".$size)
+    if ($in == $out)
+    {
+      echo "$i match - $in - $out"
+    }
+    else
+    {
+      echo "$i don't match - $in - $out"
+    }
+  }
+  $redis->set("test".$size, )
+  $redis->close();
+?>
