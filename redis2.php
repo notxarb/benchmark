32d8c87ee52e0d7ac31d9a877830c968a4917987
Braxton Huggins <braxton@delorum.com>
2012-10-03 16:35:08 -0600

    ; - theyll get you every time

--- redis2.php
+++ redis2.php
@@ -1,27 +1,27 @@
 <?php
   public static function genstring($size)
   {
-    $string = ''
+    $string = '';
     for ($i = 0; $i < $size; $i++)
     {
-      $string .= $i % 10
+      $string .= $i % 10;
     }
-    return $string
+    return $string;
   }
   $redis = new Redis();
   $redis->connect($_SERVER['CACHE2_HOST'], $_SERVER['CACHE2_PORT']);
   for($i = 64; $i < 4096; $i *= 2)
   {
-    $in = genstring($i)
-    $redis->set("test".$size, $in)
-    $out = $redis->get("test".$size)
+    $in = genstring($i);
+    $redis->set("test".$size, $in);
+    $out = $redis->get("test".$size);
     if ($in == $out)
     {
-      echo "$i match - $in - $out"
+      echo "$i match - $in - $out";
     }
     else
     {
-      echo "$i don't match - $in - $out"
+      echo "$i don't match - $in - $out";
     }
   }
   $redis->set("test".$size, )
