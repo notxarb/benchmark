c9d7a8450d89eb7c01ac7944626e4b9aa6f194af
Braxton Huggins <braxton@delorum.com>
2012-10-09 15:15:07 -0600

    serialize

--- serialize.php
+++ serialize.php
@@ -3,4 +3,11 @@ $array = array(1,2,3,"Hello World!");
 $serialized = serialize($array);
 echo $serialized;
 echo ($array == unserialize($serialized));
+
+$redis = new Redis();
+$redis->connect($_SERVER['CACHE2_HOST'], $_SERVER['CACHE2_PORT']);
+$redis->set('array', $array);
+$redis->set('serialized', $serialized);
+echo ($array == $redis->get('array'));
+echo ($array == unserialize($redis->get('serialized')));
 ?>
