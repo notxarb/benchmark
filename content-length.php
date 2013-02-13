15768c7b67a83384287f91fc3e1b3e9370f50a09
Braxton Huggins <braxton@delorum.com>
2013-02-13 14:10:00 -0700

    adding script

---
+++ content-length.php
@@ -0,0 +1,9 @@
+<?php 
+  $top = "<html><head><title>Content length test</title></head><body>\n";
+  $content = "<div>1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef</div>\n";
+  $body = str_repeat($content, 128);
+  $botton = "</body></html>\n";
+  $html = $top . $body . $bottom;
+  header("Content-Length: " . strlen($html));
+  echo $html;
+?>
